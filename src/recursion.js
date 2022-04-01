6b9a9352b5b59f1b1b3be0d43de61493562673fd
Fan <aliciafzhang@gmail.com>
2022-04-01 00:31:01 -0700

    pass isEven

--- src/recursion.js
+++ src/recursion.js
@@ -31,10 +31,24 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (typeof(array) === 'number') {
+    return array;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return (arraySum(array[0]) + arraySum(array.slice(1,array.length)));
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  }
+  if (n === 1) {
+    return false;
+  }
+  return isEven(n-2);
 };
 
 // 5. Sum all integers below a given integer.
