7b135f3b58ad225b91b7e986dd8762876a0dc917
Fan <aliciafzhang@gmail.com>
2022-04-01 17:07:07 -0700

    pass Palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -125,6 +125,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length === 0 || string.length === 1) {
+    return true;
+  }
+  if (string.substring(0,1).toUpperCase() !== string.substring(string.length -1, string.length).toUpperCase()) {
+    return false;
+  } else {
+    return palindrome(string.substring(1,string.length -1));
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
