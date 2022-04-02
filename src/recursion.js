3069d8bc3e387c285bcd409f9580c8b7cd1ff304
Fan <aliciafzhang@gmail.com>
2022-04-01 19:25:45 -0700

    pass 23 countValuesInObj

--- src/recursion.js
+++ src/recursion.js
@@ -214,6 +214,18 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+  var count = 0;
+  if (typeof(obj) !== "object"){
+    if (obj === value) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  for(item in obj) {
+    count += countValuesInObj(obj[item], value);
+  }
+  return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
